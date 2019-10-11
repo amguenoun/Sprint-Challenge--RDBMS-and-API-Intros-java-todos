0972c8099f908e7cc8ab70d4ed84becf88ff9627
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-11 14:09:29 -0400

    finsihed stretch goal to check for authorized user on updating todos

--- todos/src/main/java/com/lambda/todos/controllers/TodoController.java
+++ todos/src/main/java/com/lambda/todos/controllers/TodoController.java
@@ -5,6 +5,7 @@ import com.lambda.todos.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +16,7 @@ public class TodoController {
 	private TodoService todoService;
 
 	@PutMapping(value = "/todoid/{todoid}", consumes = {"application/json"})
-	public ResponseEntity<?> completeTodo(@PathVariable long todoid, @RequestBody Todo todo){
+	public ResponseEntity<?> updateTodo(@PathVariable long todoid, @RequestBody Todo todo){
 		todoService.update(todo, todoid);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
--- todos/src/main/java/com/lambda/todos/services/TodoServiceImpl.java
+++ todos/src/main/java/com/lambda/todos/services/TodoServiceImpl.java
@@ -1,8 +1,12 @@
 package com.lambda.todos.services;
 
 import com.lambda.todos.models.Todo;
+import com.lambda.todos.models.User;
 import com.lambda.todos.repositories.TodoRepository;
+import com.lambda.todos.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -13,6 +17,9 @@ import java.util.List;
 public class TodoServiceImpl implements TodoService {
 	@Autowired
 	private TodoRepository todoRepo;
+
+	@Autowired
+	private UserRepository userRepo;
 
 	@Override
 	public List<Todo> finalAll() {
@@ -28,20 +35,27 @@ public class TodoServiceImpl implements TodoService {
 
 	@Override
 	public Todo update(Todo todo, long id) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		User authenticatedUser = userRepo.findByUsername(authentication.getName());
 		Todo current = findTodoById(id);
+		if(current.getUser().getUserid() == authenticatedUser.getUserid()){
 
-		if(todo.getDatestarted() != null){
-			current.setDatestarted(todo.getDatestarted());
+			if(todo.getDatestarted() != null){
+				current.setDatestarted(todo.getDatestarted());
+			}
+
+			if(todo.getDescription() != null){
+				current.setDescription(todo.getDescription());
+			}
+
+			if(todo.isCompleted()){
+				current.setCompleted(todo.isCompleted());
+			}
+
+			return todoRepo.save(current);
 		}
-
-		if(todo.getDescription() != null){
-			current.setDescription(todo.getDescription());
+		else{
+			throw new EntityNotFoundException(id + " Not current user");
 		}
-
-		if(todo.isCompleted()){
-			current.setCompleted(todo.isCompleted());
-		}
-
-		return todoRepo.save(current);
 	}
 }
