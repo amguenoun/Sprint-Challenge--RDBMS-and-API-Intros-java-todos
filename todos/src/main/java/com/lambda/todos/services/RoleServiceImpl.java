b4b885eea35dba059b588f9a25493e7dbde12f74
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-11 11:45:42 -0400

    created all model services and implementations

---
+++ todos/src/main/java/com/lambda/todos/services/RoleServiceImpl.java
@@ -0,0 +1,4 @@
+package com.lambda.todos.services;
+
+public class RoleServiceImpl implements RoleService {
+}
---
+++ todos/src/main/java/com/lambda/todos/services/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.lambda.todos.services;
+
+public class UserServiceImpl implements UserService {
+}
